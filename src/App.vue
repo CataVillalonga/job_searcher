6b7974c67c63c707b2a10644cb7772c674951d01
Cata Villalonga <cata.villalonga21@gmail.com>
2023-05-02 12:39:05 +0200

    costum. css color classes for tailwing

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
 <script>
-//Vue2WAY of importing and rendering components
 import MainNav from '@/components/MainNav.vue';
 export default {
   name: 'App',
   components: {
-    MainNav //same as : MainNav:MainNav
+    MainNav
   }
 };
 </script>
 
 <template>
-  <!-- Both options are valid: -->
-  <MainNav />
-  <MainNav></MainNav>
-  <!--This is use when you want to put something inbetween the tags -->
-  <main-nav></main-nav>
   <main-nav />
-  <!-- vue also support kebab case no matter how the component is define in the components obj (line7) -->
 </template>
