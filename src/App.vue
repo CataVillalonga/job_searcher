ba4800975f7bfba230a155e3d32e3a0d80897fcf
Cata Villalonga <cata.villalonga21@gmail.com>
2023-05-02 09:52:32 +0200

    first commit barebone project of vue

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<!-- IMPORTS happen inside the script tag -->
+<script setup></script>
+
+<template>
+  <header>
+    <h1>Hello This is a test</h1>
+  </header>
+</template>
+
+<!-- Styles are global unless define otherwise (with scoped keyword), even if they are in components -->
+<style scoped></style>
