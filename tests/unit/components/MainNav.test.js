20a283a3118903000cd935873c310e0e9b65ec56
Cata Villalonga <cata.villalonga21@gmail.com>
2023-05-03 16:46:05 +0200

    fixed bug for vitest coverage display

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -3,15 +3,7 @@ import MainNav from '@/components/MainNav.vue';
 
 describe('MainNav', () => {
   it('displays company name', () => {
-    //second argument (called config obj) is not necesary, and it overwrites the data obj pass to the component (!)
-    //keep in mind : even tho is possible it might not be the best approach ... as it creates tests that are more likely to fail
-    render(MainNav, {
-      data() {
-        return {
-          companyName: 'Banana Corp'
-        };
-      }
-    });
+    render(MainNav);
     const companyName = screen.getByText('Cata Careers');
     expect(companyName).toBeInTheDocument();
   });
