54ca24f0602d82659d078b245989dc38f9c7671a
Cata Villalonga <cata.villalonga21@gmail.com>
2023-05-15 13:19:12 +0200

    v-bind directive short example

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,8 +2,8 @@
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="b mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
-        <!-- v-bind directive example -->
-        <a v-bind:href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+        <!-- v-bind directive  alternative example -->
+        <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
       </div>
     </div>
   </header>
