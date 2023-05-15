390232c28e1d3035719bfa94c42545c2a6ae7fea
Cata Villalonga <cata.villalonga21@gmail.com>
2023-05-15 13:39:16 +0200

    v-for directive example for iterating arrays (like map)

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,8 +2,17 @@
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="b mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
-        <!-- v-bind directive  alternative example -->
         <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+          <!-- v-for directive example (for iterations , same as loops or map func) -->
+            <li class="h-full ml-9" v-for=" navBarItem in navBarItems">
+              <!-- dinamically calling items on array to create navbar -->
+              <a href="/" class=" flex h-full items-center py-2.5">{{ navBarItem }}</a>
+            </li>
+
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -15,7 +24,8 @@ export default {
   data() {
     return {
       company: 'Cata Careers',
-      url : "https://careers.google.com/"
+      url : "https://careers.google.com/",
+      navBarItems:["Teams","Location","Life at Cata Corp","How we hire","Students","Jobs"],
     };
   }
 };
