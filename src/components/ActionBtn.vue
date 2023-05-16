8748cebd9dd28f7b8f7b7e472b8b780342ef0eaf
Cata Villalonga <cata.villalonga21@gmail.com>
2023-05-16 11:25:46 +0200

    basic example of css styles selected dinamically

--- src/components/ActionBtn.vue
+++ src/components/ActionBtn.vue
@@ -1,13 +1,32 @@
 <template>
-  <!-- calling the props as a variable in the btn  -->
-    <button class="rounded border-0 px-5 py-3 font-medium text-white bg-brand-blue-1 hover:shadow-blue">{{ text }}</button>
+  <!-- using v-bind to dinamically select styles within obj definition
+   connects the css class declaration to the data property value primary-->
+    <button :class="{primary:primary}">{{ text }}</button>
 </template>
 
 <script>
 export default {
   name: 'ActionBtn',
   //defining the prop(s) coming from the parent in the child 
-  props:["text"]
-
+  props:["text"],
+  data() {
+    return {
+      primary: true
+    }
+  },
 }
 </script>
+<!-- Adding scoped attr. to make sure styles stay in the component ( as css can be global) -->
+<style scoped>
+/* @apply is a tailwing directive that lets you write tailwing classes within our css declarations */
+button{
+ @apply rounded px-5 py-3 font-medium;
+}
+.primary {
+  @apply border-0 bg-brand-blue-1 text-white hover:shadow-blue
+}
+
+.secondary {
+
+}
+</style>
