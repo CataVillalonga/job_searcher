d0b78844850363b66fb10b397c9978e1c92b1630
Cata Villalonga <cata.villalonga21@gmail.com>
2023-06-07 14:20:19 +0200

    error handling with emits array

--- src/components/Shared/TextInput.vue
+++ src/components/Shared/TextInput.vue
@@ -16,6 +16,8 @@ export default {
       required: true,
     },
   },
+  //letting know the $emit event, what are correct alternatives for event handlers (a sort of typescript for events)
+  emits: ['update:modelValue'],
   methods: {
     handleInput($event) {
       this.$emit('update:modelValue', $event.target.value);
