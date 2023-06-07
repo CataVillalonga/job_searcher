a18ebd2ba8d1be8bfb6338215ed2fa8b2c9c06cb
Cata Villalonga <cata.villalonga21@gmail.com>
2023-06-07 11:38:05 +0200

    sending data properties from the child to the parent component with

--- src/components/Shared/TextInput.vue
+++ src/components/Shared/TextInput.vue
@@ -17,7 +17,11 @@ export default {
   },
   methods: {
     handleInput(e) {
+      //updating the data property on the child component (textInput.vue)
       this.value = e.target.value;
+      // informing and sending the data property to the parent component (jobSearch.vue)
+      // takes two arg ('the event', payload)
+      this.$emit('handleInput', this.value);
     },
   },
 };
